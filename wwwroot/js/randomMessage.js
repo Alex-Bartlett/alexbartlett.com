8409ecaa48e2417fc5bd02c29fa872118b2e4f23
Alex <78022094+Alex-Bartlett@users.noreply.github.com>
2021-10-01 00:29:19 +0100

    Removed inappropriate names

--- wwwroot/js/randomMessage.js
+++ wwwroot/js/randomMessage.js
@@ -1,9 +1,8 @@
 
 (function () {
     const phrases1 = ["There once was", "Never before have I seen", "Unbeknown to me, there is", "Today I found out that my neighbour is"];
-    const phrases2 = ["an elephant", "a serial killer", "a clock", "an escaped prisoner", "an Avon salesman", "a bartender", "a King", "a Princess", "a secret agent", "a frog", "a sexually-energised priest", "a harsh critic"];
+    const nouns = ["an elephant", "a serial killer", "a clock", "an escaped prisoner", "an Avon salesman", "a bartender", "a King", "a Princess", "a secret agent", "a frog", "a harsh critic", "a poor excuse of a man","a bank.", "a zoo.", "a house.", "a shed.", "a bridge.", "a helicopter.", "a prison.", "a treehouse.", "a television programme.", "a glass bottle.", "a pirate."];
     const phrases3 = ["who owned", "that fell off", "that couldn't understand the concept of", "that tried to rob", "that succesfully became", "who unfortunately tripped over", "that never spoke to", "who was attacked by","that escaped"];
-    const phrases4 = ["a bank.", "a zoo.", "a house.", "a shed.", "a bridge.", "a helicopter.", "a prison.", "a treehouse.", "a television programme.", "a glass bottle.", "a pirate."];
     const phrases5 = ["You don't hear stories like that these days.", "It was the talk of the town.", "What a disaster that turned out to be!", "I was shocked to hear that news!", "But that's no surprise.", "But thats just the start!", "It's a short story, sorry.", "You're probably not interested.", "Please leave a like and subscribe if you enjoyed!", "Follow @alexbartlett7710 for more stories!"]
 
     let msg = [];
@@ -17,13 +16,13 @@
                 currentList = phrases1;
                 break;
             case 1:
-                currentList = phrases2;
+                currentList = nouns;
                 break;
             case 2:
                 currentList = phrases3;
                 break;
             case 3:
-                currentList = phrases4;
+                currentList = nouns;
                 break;
             case 4:
                 currentList = phrases5;
