f8f26240ec35f6d40c2ecbd7bbc77218b2088a64
Alex <78022094+Alex-Bartlett@users.noreply.github.com>
2021-10-01 00:46:48 +0100

    Add punctuation code

--- wwwroot/js/randomMessage.js
+++ wwwroot/js/randomMessage.js
@@ -1,13 +1,14 @@
 
 (function () {
     const phrases1 = ["There once was", "Never before have I seen", "Unbeknown to me, there is", "Today I found out that my neighbour is"];
-    const nouns = ["an elephant", "a serial killer", "a clock", "an escaped prisoner", "an Avon salesman", "a bartender", "a King", "a Princess", "a secret agent", "a frog", "a harsh critic", "a poor excuse of a man","a bank.", "a zoo.", "a house.", "a shed.", "a bridge.", "a helicopter.", "a prison.", "a treehouse.", "a television programme.", "a glass bottle.", "a pirate."];
+    const nouns = ["an elephant", "a serial killer", "a clock", "an escaped prisoner", "an Avon salesman", "a bartender", "a King", "a Princess", "a secret agent", "a frog", "a harsh critic", "a poor excuse of a man","a bank", "a zoo", "a house", "a shed", "a bridge", "a helicopter", "a prison", "a treehouse", "a television programme", "a glass bottle", "a pirate"];
     const phrases3 = ["who owned", "that fell off", "that couldn't understand the concept of", "that tried to rob", "that succesfully became", "who unfortunately tripped over", "that never spoke to", "who was attacked by","that escaped"];
+	const punctuation = [".", ".", ".", ".","!","!", "?"];
     const phrases5 = ["You don't hear stories like that these days.", "It was the talk of the town.", "What a disaster that turned out to be!", "I was shocked to hear that news!", "But that's no surprise.", "But thats just the start!", "It's a short story, sorry.", "You're probably not interested.", "Please leave a like and subscribe if you enjoyed!", "Follow @alexbartlett7710 for more stories!"]
 
     let msg = [];
 
-    for (let i=0; i<5; i++){
+    for (let i=0; i<6; i++){
 
         //Set the current list according to index
         let currentList;
@@ -24,7 +25,10 @@
             case 3:
                 currentList = nouns;
                 break;
-            case 4:
+			case 4:
+				currentList = punctuation;
+				break;
+            case 5:
                 currentList = phrases5;
                 break;
             default:
@@ -35,8 +39,21 @@
         //Add phrase to the message
         msg.push(phrase);
     }
+
     //Join the array together into a string
-    finishedMsg = msg.join(" ");
+    let finishedMsg = msg.join(" ");
+
+	let separated = finishedMsg.split("");
+	//Remove spaces before punctuation
+	separated.forEach((word, index) => {
+		if (punctuation.includes(word) && separated[index-1] == " "){
+			separated.splice(index-1, 1);
+		}
+	})
+	//Rejoin message
+	finishedMsg = separated.join("");
+
+
     //Display the message
     document.getElementById("message").textContent = finishedMsg;
 }())
