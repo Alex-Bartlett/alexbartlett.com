c3ebe511b1abf8572faafb45fe467453cc154b80
Alex <78022094+Alex-Bartlett@users.noreply.github.com>
2021-10-01 01:05:44 +0100

    Finished

--- wwwroot/js/randomMessage.js
+++ wwwroot/js/randomMessage.js
@@ -1,10 +1,10 @@
 
 (function () {
     const phrases1 = ["There once was", "Never before have I seen", "Unbeknown to me, there is", "Today I found out that my neighbour is"];
-    const nouns = ["an elephant", "a serial killer", "a clock", "an escaped prisoner", "an Avon salesman", "a bartender", "a King", "a Princess", "a secret agent", "a frog", "a harsh critic", "a poor excuse of a man","a bank", "a zoo", "a house", "a shed", "a bridge", "a helicopter", "a prison", "a treehouse", "a television programme", "a glass bottle", "a pirate"];
-    const phrases3 = ["who owned", "that fell off", "that couldn't understand the concept of", "that tried to rob", "that succesfully became", "who unfortunately tripped over", "that never spoke to", "who was attacked by","that escaped"];
+    const nouns = ["an elephant", "a serial killer", "a clock", "an escaped prisoner", "an Avon salesman", "a bartender", "a King", "a Princess", "a secret agent", "a frog", "a harsh critic", "a poor excuse of a man","a bank", "a zoo", "a house", "a shed", "a bridge", "a helicopter", "a prison", "a treehouse", "a television programme", "a glass bottle", "a pirate", "a lonely man", "a volcano", "a historical artifact"];
+    const phrases3 = ["who clearly hated", "that just knew how to please", "that did some bad things to", "who owned", "that fell off", "that couldn't understand the concept of", "that tried to rob", "that succesfully became", "who unfortunately tripped over", "that never spoke to", "who was attacked by","that escaped"];
 	const punctuation = [".", ".", ".", ".","!","!", "?"];
-    const phrases5 = ["You don't hear stories like that these days.", "It was the talk of the town.", "What a disaster that turned out to be!", "I was shocked to hear that news!", "But that's no surprise.", "But thats just the start!", "It's a short story, sorry.", "You're probably not interested.", "Please leave a like and subscribe if you enjoyed!", "Follow @alexbartlett7710 for more stories!"]
+    const phrases5 = ["Put that in your prayers tonight.", "Bet you'll be coming back for more!", "You don't hear stories like that these days.", "It was the talk of the town.", "What a disaster that turned out to be!", "I was shocked to hear that news!", "But that's no surprise.", "But thats just the start!", "It's a short story, sorry.", "You're probably not interested.", "Please leave a like and subscribe if you enjoyed!", "I bet you feel satisfied now."]
 
     let msg = [];
 
