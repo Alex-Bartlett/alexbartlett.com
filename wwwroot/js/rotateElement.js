e8e76e9c8cad38a7d62b62ac7201cf3de0152488
Alex <78022094+Alex-Bartlett@users.noreply.github.com>
2021-11-05 01:48:05 +0000

    Add UpSkill portfolio piece

--- wwwroot/js/rotateElement.js
+++ wwwroot/js/rotateElement.js
@@ -1,13 +1,33 @@
-function move() {
-    var interval = setInterval(anim, 5);
-    let stop = false;
-    const elem = document.getElementById("rotate");
-    function anim() {
-        if (stop === true) {
-            clearInterval(interval);
-        }
-        else {
-            elem.style.translate = "rotate(5)";
-        }
-    }
+if (document.getElementById("rotateObject")) {
+	let object = document.getElementById("rotateObject").animate(
+		[{ transform: "translate(-50%, -50%)" },
+		{ transform: "translate(-50%, 150%)" },
+		{ transform: "translate(-50%, -50%)" }],
+		{
+			duration: 1000,
+			iterations: Infinity,
+			easing: "ease-in-out"
+		}
+	);
+}
+if (document.getElementById("rotateText")) {
+	let text = document.getElementById("rotateText").animate(
+		[{ transform: "rotate(0deg)" },
+		{ transform: "rotate(360deg)" }],
+		{
+			duration: 2000,
+			iterations: Infinity
+		}
+	)
+}
+if (document.getElementById("growText")) {
+	document.getElementById("growText").animate(
+		[{ fontsize: "1em" },
+		{ fontsize: "4em" },
+		{ fontsize: "1em;" }],
+		{
+			duration: 2000,
+			iterations: Infinity
+		}
+	)
 }
