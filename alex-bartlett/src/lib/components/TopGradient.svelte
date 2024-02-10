c8cd4be4a495bd0a5f23d4c3e879690eea6e5e15
Alex <78022094+Alex-Bartlett@users.noreply.github.com>
2024-02-10 21:11:27 +0000

    Add svelte project

---
+++ alex-bartlett/src/lib/components/TopGradient.svelte
@@ -0,0 +1,9 @@
+<div
+	class="w-1/4 h-40 bg-gradient-to-br from-purple-400 to-indigo-700 custom"
+></div>
+
+<style>
+	.custom {
+		background: radial-gradient(closest-side);
+	}
+</style>
---
+++ alex-bartlett/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import "../app.css";
+	import TopGradient from "../lib/components/TopGradient.svelte";
+</script>
+
+<header class="max-w-full">
+	<TopGradient />
+</header>
+
+<slot />
