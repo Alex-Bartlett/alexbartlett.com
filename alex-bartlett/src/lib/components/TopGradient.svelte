20719273729cba18e4cfcfad1df2ce59cc74c63f
Alex <78022094+Alex-Bartlett@users.noreply.github.com>
2024-02-12 12:17:14 +0000

    Add gradient component

--- alex-bartlett/src/lib/components/TopGradient.svelte
+++ alex-bartlett/src/lib/components/TopGradient.svelte
@@ -1,9 +1,32 @@
-<div
-	class="w-1/4 h-40 bg-gradient-to-br from-purple-400 to-indigo-700 custom"
-></div>
+<div class="scale-150 gradient-main -z-10">
+	<div class="gradient-container">
+		<div class="g g1"></div>
+		<div class="g g2"></div>
+		<div class="g g3"></div>
+	</div>
+</div>
 
 <style>
-	.custom {
-		background: radial-gradient(closest-side);
+	.gradient-main {
+		position: absolute;
+	}
+	.gradient-container {
+		transform: translate(-200px, -100px);
+	}
+	.g {
+		position: absolute;
+		height: 400px;
+		width: 400px;
+	}
+	.g1 {
+		background: radial-gradient(#8f498d8c, #712c8761, #fff0, #fff0);
+	}
+	.g2 {
+		transform: translate(150px, -100px);
+		background: radial-gradient(#fc466a79, #fc466a38, #fff0, #fff0);
+	}
+	.g3 {
+		transform: translate(300px, -150px) scale(0.8, 0.8);
+		background: radial-gradient(#fc7a4679, #fc7a4638, #fff0, #fff0);
 	}
 </style>
--- alex-bartlett/src/routes/+page.svelte
+++ alex-bartlett/src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<h1>Alex Bartlett</h1>
+<main
+	class="w-1/2 h-full flex flex-col items-center border-l-2 border-r-2 border-gray-200/50 my-auto"
+>
+	<h1>Alex Bartlett</h1>
+</main>
